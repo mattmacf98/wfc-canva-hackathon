daffe4b9d83813a7b1cee2a62085ab2a2ee296e3
mattmacf98 <macfarqu@amazon.com>
2024-08-07 23:26:01 -0400

    set up environment detection

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,6 @@
-export const backendHost = "https://canva-hackathon-backend-33cb89cc3ea5.herokuapp.com"
-// export const backendHost = "http://127.0.0.1:3001"
+const environment = window.location.hostname === '127.0.0.1' ? 'development' : 'production'
+const ENV_TO_BACKEND_HOST = {
+    "production": "https://canva-hackathon-backend-33cb89cc3ea5.herokuapp.com",
+    "development": "http://127.0.0.1:3001"
+}
+export const backendHost = ENV_TO_BACKEND_HOST[environment]
